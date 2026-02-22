02a29d8693e7b2cb30371a1c3c164c7f75e448d7
Nisanur <nisanursinkayaa@gmail.com>
2026-02-22 15:24:26 +0300

    Inıtial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+import glob
+import streamlit as st
+import plotly.express as px
+
+from nltk.sentiment import SentimentIntensityAnalyzer
+
+filepaths = sorted(glob.glob("diary/*.txt"))
+
+analyzer = SentimentIntensityAnalyzer()
+
+positivity = []
+negativity = []
+for filepath in filepaths:
+    with open(filepath) as file:
+        content = file.read()
+    scores = analyzer.polarity_scores(content)
+    positivity.append(scores['pos'])
+    negativity.append(scores['neg'])
+
+dates = [name.strip(".txt").strip("diary/") for name in filepaths]
+
+st.title("Diary Tone")
+st.subheader("Positivity")
+pos_figure = px.line(x=dates, y=positivity,
+                     labels ={ "x": "dates", "y": "positivity"})
+st.plotly_chart(pos_figure)
+
+st.subheader("Negativity")
+neg_figure = px.line(x=dates, y=negativity,
+                     labels ={ "x": "dates", "y": "negativity"})
+st.plotly_chart(neg_figure)
+
+
